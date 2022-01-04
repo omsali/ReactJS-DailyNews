fbd54a2046d9362743d29e6fb9eaad0fa3e8afeb
SALI OM RAVINDRA <74290304+omsali@users.noreply.github.com>
2022-01-04 12:41:27 +0530

    added infinite scroll , top loading bar

--- src/compnents/NewsItem.js
+++ src/compnents/NewsItem.js
@@ -8,15 +8,17 @@ export class NewsItem extends Component {
         return (
             <div >
                 <div className="card newsItem my-3" >
+                <div style={{display: 'flex', justifyContent: 'flex-end', position: 'absolute', right : '0'}}>
+                <span className=" badge rounded-pill bg-danger" >
+                    {source}
+                </span>
+                </div>
                 <img src={imageUrl} style={{height : '200px'}} className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <span className="position-absolute translate-middle badge rounded-pill bg-danger" style={{left : '90%', zIndex : '1', top : '2%'}}>
-                        {source}
-                    </span>
                     <h5 className="card-title">{title}...</h5>
                     <p className="card-text">{description}...</p>
                     <p className="card-text"><small className="text-muted">{!author?"Unknown":author} | Updated: {new Date(date).toGMTString()}</small></p>
-                    <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
                 </div>
                 </div>
             </div>
