806640e70b3c022f689959529113e59c0093e7f3
SALI OM RAVINDRA <74290304+omsali@users.noreply.github.com>
2022-01-04 12:42:03 +0530

    added infinite scroll, added top laoding bar

--- src/compnents/Navbar.js
+++ src/compnents/Navbar.js
@@ -14,17 +14,17 @@ export class Navbar extends Component {
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/general">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link active mx-3" aria-current="page" to="/general">Home</Link></li>
             <div className="dropdown">
                 <button className="btn btn-dark text-light dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Categories</button>
-                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li className="nav-item"><Link className="nav-link" to="/business" style={{color: 'black'}}>Business</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/entertainment" style={{color: 'black'}} >Entertainment</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/general" style={{color: 'black'}}>General</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/health" style={{color: 'black'}}>Health</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/science" style={{color: 'black'}}>Science</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/sports" style={{color: 'black'}}>Sports</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/technology" style={{color: 'black'}}>Technology</Link></li>
+                <ul className="dropdown-menu bg-dark" aria-labelledby="dropdownMenuButton1">
+                    <li className="nav-item"><Link className="nav-link" to="/business" >Business</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/entertainment"  >Entertainment</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/general" >General</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/health" >Health</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/science" >Science</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/sports" >Sports</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/technology" >Technology</Link></li>
                 </ul>
             </div>
         </ul>
